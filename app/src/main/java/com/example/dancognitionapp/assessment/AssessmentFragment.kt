32badd67086b806dd2e2cda4e2b1aab683aa390b
Tony Kazanjian <tony.kazanjian@gmail.com>
2023-07-19 17:32:57 -0700

    adds code to block backpress calls on a real assessment

--- app/src/main/java/com/example/dancognitionapp/assessment/AssessmentFragment.kt
+++ app/src/main/java/com/example/dancognitionapp/assessment/AssessmentFragment.kt
@@ -3,6 +3,7 @@ package com.example.dancognitionapp.assessment
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 
 /**
@@ -10,6 +11,18 @@ import androidx.fragment.app.Fragment
  * implement
  */
 abstract class AssessmentFragment: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val isPractice = requireActivity().intent.getBooleanExtra(AssessmentActivity.IS_PRACTICE, false)
+        //blocks backpresses if this is a real assessment
+        if (!isPractice) {
+            val backPressedCallback = object: OnBackPressedCallback(isPractice) {
+                override fun handleOnBackPressed() {}
+            }
+            requireActivity().onBackPressedDispatcher.addCallback(this, backPressedCallback)
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE

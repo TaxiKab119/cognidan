35a07eb670681f874e339f05bf3d5326b2559433
TaxiKab119 <cs.k.alexanderbalan@gmail.com>
2023-07-20 18:48:37 -0400

    Rebase onto develop

--- app/src/main/java/com/example/dancognitionapp/assessment/bart/BartFragment.kt
+++ app/src/main/java/com/example/dancognitionapp/assessment/bart/BartFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import com.example.dancognitionapp.R
 import com.example.dancognitionapp.assessment.AssessmentFragment
+import com.example.dancognitionapp.bart.BartTestScreen
 
 class BartFragment: AssessmentFragment() {
 
@@ -18,7 +21,7 @@ class BartFragment: AssessmentFragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_compose_host, container, false)
         view.findViewById<ComposeView>(R.id.compose_root).setContent {
-            Text(text = "Put BART screen here")
+            BartTestScreen(Modifier.fillMaxSize())
         }
         return view
     }

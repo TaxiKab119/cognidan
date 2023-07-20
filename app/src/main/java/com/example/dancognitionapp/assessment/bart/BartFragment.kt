c3effd7bda873371c817b4bedcc343a49a7009fa
TaxiKab119 <cs.k.alexanderbalan@gmail.com>
2023-07-20 18:55:24 -0400

    Add theme for BART screen

--- app/src/main/java/com/example/dancognitionapp/assessment/bart/BartFragment.kt
+++ app/src/main/java/com/example/dancognitionapp/assessment/bart/BartFragment.kt
@@ -5,12 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import com.example.dancognitionapp.R
 import com.example.dancognitionapp.assessment.AssessmentFragment
 import com.example.dancognitionapp.bart.BartTestScreen
+import com.example.dancognitionapp.ui.theme.DanCognitionAppTheme
 
 class BartFragment: AssessmentFragment() {
 
@@ -21,7 +21,9 @@ class BartFragment: AssessmentFragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_compose_host, container, false)
         view.findViewById<ComposeView>(R.id.compose_root).setContent {
-            BartTestScreen(Modifier.fillMaxSize())
+            DanCognitionAppTheme() {
+                BartTestScreen(Modifier.fillMaxSize())
+            }
         }
         return view
     }

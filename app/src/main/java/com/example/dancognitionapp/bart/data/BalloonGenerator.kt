1065036a45d0126805c3287b3d00f1d96ad3d100
TaxiKab119 <cs.k.alexanderbalan@gmail.com>
2023-07-21 16:21:53 -0400

    Create Balloon model class and BalloonGenerator
    The BalloonGenerator class has a method that returns a LinkedList with NUMBER_OF_BALLOONS balloons.

---
+++ app/src/main/java/com/example/dancognitionapp/bart/data/BalloonGenerator.kt
@@ -0,0 +1,23 @@
+package com.example.dancognitionapp.bart.data
+
+import com.example.dancognitionapp.bart.model.Balloon
+import java.util.LinkedList
+import kotlin.random.Random
+
+const val MIN_INFLATIONS: Int = 1
+const val MAX_INFLATIONS: Int = 20
+const val NUMBER_OF_BALLOONS: Int = 20
+
+class BalloonGenerator {
+    fun getBalloonLinkedList(): LinkedList<Balloon> {
+
+        val balloonLinkedList = LinkedList<Balloon>()
+
+        for (balloonNumber in 1..NUMBER_OF_BALLOONS) {
+            val maxInflation = Random.nextInt(MIN_INFLATIONS, MAX_INFLATIONS)
+            balloonLinkedList.add(Balloon(balloonNumber, maxInflation))
+        }
+
+        return balloonLinkedList
+    }
+}
---
+++ app/src/main/java/com/example/dancognitionapp/bart/model/Balloon.kt
@@ -0,0 +1,6 @@
+package com.example.dancognitionapp.bart.model
+
+data class Balloon(
+    val listPosition: Int,
+    val maxInflations: Int
+)

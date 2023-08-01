1619973d27a0edea7fef7a0e4b345f7f7954669a
TaxiKab119 <cs.k.alexanderbalan@gmail.com>
2023-08-01 15:59:12 -0400

    Update generateChars method, Bugs remain
    This update has a (mostly) functioning implementation of the code to generate nBack order. Most of the time it is returning a 1:2 validity ratio (target-foil ratio) but occasionally bugs come up in the two-back and three-back conditions where an additional target is created unintentionally. This bug appears to be in the initial block which places targets and buddies. adjacentChars must also be updated for case where i-n to i+n contains all possible stimuli as this causes an exception.

--- app/src/main/java/com/example/dancognitionapp/nback/NBackGenerator.kt
+++ app/src/main/java/com/example/dancognitionapp/nback/NBackGenerator.kt
@@ -1,11 +1,122 @@
 package com.example.dancognitionapp.nback
 
+import timber.log.Timber
 import java.util.LinkedList
+import kotlin.random.Random
+import kotlin.random.nextInt
 
-class NBackGenerator {
+private const val NUMBER_OF_PRESENTATIONS = 12
+class NBackGenerator(private val testType: NBackType) {
 
-    private fun generateNBackPresentationOrder(n: Int): LinkedList<NBackItem> {
-
-
+    val items = LinkedList<NBackItem>()
+    var validityRatio: Double? = null
+    init {
+        generateNBackPresentationOrder()
+        getValidityRatio()
     }
+
+    private fun generateValidChars(): CharArray {
+        val stimuli = listOf('A', 'B', 'C', 'D', 'E')
+        val chars = CharArray(NUMBER_OF_PRESENTATIONS) {'Z'} // Z is a placeholder (never used as a stimuli)
+        val targetNumber = NUMBER_OF_PRESENTATIONS / 3
+        val n = testType.value
+        val random = Random(System.currentTimeMillis())
+
+        /**This block places targets and "target buddies" randomly throughout presentation order.
+         * This ensures there are exactly [targetNumber] targets.
+         * */
+        val excludedValues = mutableListOf<Int>()
+        for (i in 1..targetNumber) {
+            var targetValue = stimuli.random(random)
+            if (i == 1) {
+                // targetIndex can be as low as n so that the target-buddy can still be placed
+                val targetIndex = Random.nextInt(n until NUMBER_OF_PRESENTATIONS)
+                chars[targetIndex] = targetValue
+                chars[targetIndex - n] = targetValue
+                excludedValues.addAll(listOf(targetIndex, targetIndex - n, targetIndex + n))
+            } else {
+                val possibleValues = (n until NUMBER_OF_PRESENTATIONS)
+                    .filterNot { it in excludedValues }
+                val targetIndex = possibleValues.random(random)
+
+                // resolve bug of repeating a letter and creating more targets than desired
+                val repeatedLetter = targetValue == chars[targetIndex] + n
+                        || targetValue == chars[targetIndex - (n - 1)]
+                if (repeatedLetter) {
+                    val modifiedStimuli = stimuli.toMutableList()
+                    modifiedStimuli.remove(targetValue)
+                    targetValue = modifiedStimuli.random(random)
+                }
+                chars[targetIndex] = targetValue
+                chars[targetIndex - n] = targetValue
+                excludedValues.addAll(listOf(targetIndex, targetIndex - n, targetIndex + n))
+            }
+        }
+        println("Chars before replacing Zs: ${chars.contentToString()}")
+
+        /**This code checks to see if a given index is empty. Then it chooses a letter.
+         * If the letter it wants to place is adjacent (will mess up the # of targets)
+         * it chooses a different letter.
+         *
+         * The end goal of this block is to ensure all spots are filled with stimuli in the presentation order
+         * */
+        val endLoop = chars.size - 1
+        for (i in 0..endLoop) {
+            if (chars[i] == 'Z') {
+                val adjacentChars = (i - n..i + n).mapNotNull() { chars.getOrNull(it) }
+                println("Adjacent Chars: $adjacentChars")
+                val foilOptions = stimuli.filterNot { it in adjacentChars }
+                chars[i] = foilOptions.random(random)
+            }
+        }
+        println("Final Chars List: ${chars.contentToString()}")
+        return chars
+    }
+
+    private fun generateNBackPresentationOrder() {
+        val chars = generateValidChars()
+        val endLoop = chars.size - 1
+
+        for (i in 0..endLoop) {
+            if (i >= testType.value) {
+                items.add(
+                    NBackItem(position = i + 1, letter = chars[i], key = chars[i - testType.value])
+                )
+            } else {
+                items.add(NBackItem(position = i + 1, letter = chars[i]))
+            }
+        }
+    }
+
+    private fun getValidityRatio() {
+        var counter = 0.0
+        for (item in items) {
+            if (item.isValid()) {
+                counter++
+            }
+        }
+        validityRatio = counter / items.size
+    }
+
+}
+
+enum class NBackType(val value: Int) {
+    N_1(1),
+    N_2(2),
+    N_3(3)
+}
+
+fun main() {
+    val oneBack = NBackGenerator(NBackType.N_1)
+    println("Validity: ${oneBack.validityRatio}")
+    println("------------------------------------")
+    println("")
+
+    val twoBack = NBackGenerator(NBackType.N_2)
+    println("Validity: ${twoBack.validityRatio}")
+    println("------------------------------------")
+    println("")
+
+    val threeBack = NBackGenerator(NBackType.N_3)
+    println("Validity: ${threeBack.validityRatio}")
 }
---
+++ app/src/main/java/com/example/dancognitionapp/nback/NBackItem.kt
@@ -0,0 +1,21 @@
+package com.example.dancognitionapp.nback
+
+import java.util.UUID
+
+data class NBackItem(
+    val id: UUID = UUID.randomUUID(),
+    val position: Int,
+    val letter: Char,
+    val key: Char? = null
+
+) {
+    fun isValid() = letter == key
+}
+
+
+enum class NBackKey {
+    HIT,
+    MISS,
+    FALSE_ALARM,
+    CORRECT_REJECTION
+}

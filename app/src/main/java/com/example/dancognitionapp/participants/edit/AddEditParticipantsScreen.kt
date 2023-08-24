97fa29e6d22fa85e4cdb32d07b8ff4c9d0f44465
TaxiKab119 <cs.k.alexanderbalan@gmail.com>
2023-08-24 17:27:19 -0400

    Clean up imports

--- app/src/main/java/com/example/dancognitionapp/participants/edit/AddEditParticipantsScreen.kt
+++ app/src/main/java/com/example/dancognitionapp/participants/edit/AddEditParticipantsScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember

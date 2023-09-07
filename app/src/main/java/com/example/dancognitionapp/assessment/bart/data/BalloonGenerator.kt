0d9bc5393f9cf6b229538ac42bc69e3d7c854e29
TaxiKab119 <cs.k.alexanderbalan@gmail.com>
2023-09-07 14:26:11 -0400

    Update BalloonGenerator
    Intended behaviour should be to have balloons with a max inflation of 0 to 19. For example, a balloon with a max inflations of 0 would only require 1 inflation to pop. A balloon with a max inflations of 19 would require 20 inflations to pop.

--- app/src/main/java/com/example/dancognitionapp/assessment/bart/data/BalloonGenerator.kt
+++ app/src/main/java/com/example/dancognitionapp/assessment/bart/data/BalloonGenerator.kt
@@ -2,9 +2,10 @@ package com.example.dancognitionapp.assessment.bart.data
 
 import java.util.LinkedList
 import kotlin.random.Random
+import kotlin.random.nextInt
 
-private const val MIN_INFLATIONS: Int = 1
-private const val MAX_INFLATIONS: Int = 20
+private const val MIN_INFLATIONS: Int = 0
+private const val MAX_INFLATIONS: Int = 19
 private const val NUMBER_OF_BALLOONS: Int = 20
 
 class BalloonGenerator {
@@ -16,7 +17,7 @@ class BalloonGenerator {
 
     private fun generateBalloons() {
         for (balloonNumber in 1..NUMBER_OF_BALLOONS) {
-            val maxInflation = Random.nextInt(MIN_INFLATIONS, MAX_INFLATIONS + 1) // REVIEW
+            val maxInflation = Random.nextInt(MIN_INFLATIONS..MAX_INFLATIONS)
             balloons.add(Balloon(balloonNumber, maxInflation))
         }
     }
